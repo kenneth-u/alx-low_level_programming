236e7b67ef7b204c995b5cd1c0018df669a4ba59
Kenneth Ukomadu <you@example.com>
2022-10-13 14:13:01 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,2 +1,10 @@
-#!/bin/bash
-puts("\"Programming is like building a multilingual puzzle");
+#include <stdio.h>
+**/
+ * main - main block
+ * Return: 0
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0)
+}
