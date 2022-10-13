7c43175026cde5fe7cc486fd05dd88e89a65fe4a
Kenneth Ukomadu <you@example.com>
2022-10-13 15:11:26 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,11 @@
+#!/bin/bash
 #include <stdio.h>
-**/
- ** main - entry point
- ** Return: always 0
- **/
+/**
+ * main - entry point
+ *
+ * Return: always (0)
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
 }
