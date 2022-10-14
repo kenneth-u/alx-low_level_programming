32d9378244f3ca09df4180b0700eb1d0729bd5a0
Kenneth Ukomadu <you@example.com>
2022-10-13 21:46:32 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,16 @@
+#include <stdio.h>
 #include <unistd.h>
+
 /**
- * main - Entry point
+ * main - this program prints a sentence and date
+ * followed by a new line, to the standard error.
  *
- * Return: Always 1
+ * Return: 1 for success
  */
+
 int main(void)
 {
 	write(1, "and that piece of art is useful" - Dora Korpar, 2015-10-19\n", 59);
-	return (1)
 
+	return (1);
 }
