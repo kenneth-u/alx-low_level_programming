90fe783dcf2e73dbd01dd5c1a01bb49b44a8b537
Kenneth Ukomadu <you@example.com>
2022-10-13 21:57:43 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,15 +2,11 @@
 #include <unistd.h>
 
 /**
- * main - this program prints a sentence and date
- * followed by a new line, to the standard error.
- *
- * Return: 1 for success
+ * main - print out the last part of a quote in a standard error.
+ * Return: 1 for success.
  */
-
 int main(void)
 {
-	write(1, "and that piece of art is useful" - Dora Korpar, 2015-10-19\n", 59);
-
+	write(2, "and that piece of art is useful" - Dora Korpar, 2015-10-19\n", 58);
 	return (1);
 }
