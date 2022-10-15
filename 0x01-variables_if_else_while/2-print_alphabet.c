c8bec1bef243d7ab9e897fbbb4d7a7fe34b77d66
Kenneth Ukomadu <you@example.com>
2022-10-15 13:32:31 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ *main - Entry point
+ *
+ *  alphabet in lowercase, followed by a new line
+ *  Return: 0 (Success)
+ */
+int main(void)
+{
+	char i;
+
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+
+	return (0);
+}
