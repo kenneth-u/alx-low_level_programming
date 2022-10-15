e0b0be96f175411835acfbf1ce5aa7abc9707abb
Kenneth Ukomadu <you@example.com>
2022-10-15 13:52:19 -0700

    3-print_alphabets.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -3,18 +3,23 @@
 /**
  *main - Entry point
  *
- *  alphabet in lowercase, followed by a new line
+ *  alphabet in lowercas eand uppercase, followed by a new line
  *  Return: 0 (Success)
  */
 int main(void)
 {
 	char i;
+	char x;
 
 	for (i = 'a'; i <= 'z'; i++)
 	{
 		putchar(i);
 	}
+	for (x = 'A; x <= 'Z'; x++)
+	{
+		putchar(x);
+	}
 	putchar('\n');
 
 	return (0);
-
+}
