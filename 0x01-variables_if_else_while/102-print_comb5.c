cbf029e8bce280e0f256d6f8e4cd4f0ffcdf8b45
Kenneth Ukomadu <you@example.com>
2022-10-15 18:13:54 -0700

    102-print_comb5.c

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * main - main block
+ * combinations of two two-digit numbers.
+ * Numbers should be printed in ascending order
+ * You can only use the putchar function
+ * You can only use putchar six times maximum in
+ * You are not allowed to use any variable of
+ * All your code should be in the main function
+ * Return: 0
+ */
+int main(void)
+{
+	int c, i, k, j;
+
+	for (c = 48; c <= 57; c++)
+	{
+		for (i = 48; i <= 57; i++)
+		{
+			for (k = 48; k <= 57; k++)
+			{
+				for (j = 48; j <= 57; j++)
+				{
+					if (((k + j) > (c + i) && k >= c) || c < k)
+					{
+						putchar(c);
+						putchar(i);
+						putchar(' ');
+						putchar(k);
+						putchar(j);
+
+		if (c + i + k + j == 227 && c == 57)
+		{
+			break;
+		}
+		else
+		{
+			putchar(',');
+			putchar(' ');
+		}
+					}
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
