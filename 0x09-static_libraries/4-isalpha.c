c5987631d3bf79987c1c9c118f49bc51a26a6985
kenneth-u <113608399+kenneth-u@users.noreply.github.com>
2022-11-07 10:17:12 +0100

    Create 4-isalpha.c

---
+++ 0x09-static_libraries/4-isalpha.c
@@ -0,0 +1,45 @@
+#include "main.h"
+
+/**
+ * _isalpha - Check for alphabetic character.
+ * @c: single letter input
+ * Return: 1 if letter, lowercase or uppercase, 0 otherwise
+ */
+
+int _isalpha(int c)
+{
+	if ((c > 64 && c < 91) || (c > 96 && c < 123))
+		return (1);
+	else
+		return (0);
+}
+
+4-strpbrk.c
+
+#include <stdlib.h>
+#include "main.h"
+/**
+ * _strpbrk - searchs a string for any of a set of bytes
+ *
+ * @s: the string to search
+ * @accept: the bytes to search for
+ *
+ * Return: a pointer to the byte in @s that matches one of the bytes in
+ * @accept, or NULL if no such byte is found.
+ */
+
+char *_strpbrk(char *s, char *accept)
+{
+	int i;
+
+	while (*s)
+	{
+		for (i = 0; accept[i]; i++)
+		{
+			if (*s == accept[i])
+				return (s);
+		}
+		s++;
+	}
+	return (NULL);
+}
