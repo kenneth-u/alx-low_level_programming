853ee65ed05e18c370ecd2b964f965427e5d3964
Kenneth Ukomadu <you@example.com>
2022-11-07 02:08:28 -0800

    4-isalpha.c

--- 0x09-static_libraries/4-isalpha.c
+++ 0x09-static_libraries/4-isalpha.c
@@ -13,33 +13,3 @@ int _isalpha(int c)
 	else
 		return (0);
 }
-
-4-strpbrk.c
-
-#include <stdlib.h>
-#include "main.h"
-/**
- * _strpbrk - searchs a string for any of a set of bytes
- *
- * @s: the string to search
- * @accept: the bytes to search for
- *
- * Return: a pointer to the byte in @s that matches one of the bytes in
- * @accept, or NULL if no such byte is found.
- */
-
-char *_strpbrk(char *s, char *accept)
-{
-	int i;
-
-	while (*s)
-	{
-		for (i = 0; accept[i]; i++)
-		{
-			if (*s == accept[i])
-				return (s);
-		}
-		s++;
-	}
-	return (NULL);
-}
